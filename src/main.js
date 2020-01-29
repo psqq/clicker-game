8b09e0aaa9488c5c4addeb0976c461c638a223c1
Sergey Pestov <pestov-sa@yandex.ru>
2020-01-29 12:15:09 +0300

    jc

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Victor from 'victor';
-
-let v1 = new Victor(1, 1);
-let v2 = v1.clone().add(v1);
-
-export default () => {
-  document.body.innerHTML = 'Hello, World!';
-  console.log(v1, v2);
-};
