0c71ba72112e077337d15136c727fc078134c826
Sergey Pestov <pestov-sa@yandex.ru>
2020-01-29 12:01:57 +0300

    parcel-plugin-clean-dist

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -1500,7 +1500,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44977" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45832" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
